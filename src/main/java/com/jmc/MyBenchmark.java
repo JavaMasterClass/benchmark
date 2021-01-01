ff9a6b8fd6547905879a2f308bee270f970b8d86
Anuj Jain <anuj@Anujs-MacBook-Pro.local>
2021-01-01 22:06:45 +0530

    minor improvements to fib_dp

--- src/main/java/com/jmc/MyBenchmark.java
+++ src/main/java/com/jmc/MyBenchmark.java
@@ -11,7 +11,6 @@ import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
@@ -22,9 +21,8 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Fork(1)
-@Measurement(time = 15, iterations = 1)
+@Measurement(time = 15, iterations = 2)
 @Warmup(time = 15, iterations = 1)
-@Threads(2)
 public class MyBenchmark {
 
     @Param("100000")
@@ -32,17 +30,15 @@ public class MyBenchmark {
 
     @Benchmark
     public int fib_dp() { 
-        int f[] = new int[n + 1]; 
-        int i; 
-  
+        int f[] = new int[n + 1];   
         f[0] = 0;
-  
         if (n > 0) {
-            f[1] = 1; 
+            f[1] = 1;
+        }   
   
-            for (i = 2; i <= n; i++)
-                f[i] = f[i - 1] + f[i - 2]; 
-        } 
+        for (int i = 2; i <= n; i++)
+            f[i] = f[i - 1] + f[i - 2];  
+                
         return f[n]; 
     }
 
